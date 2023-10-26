a42d6b98822dffaed3a73cb4ffe8ceedd77d8c62
QuFranklin <qufrank0513@gmail.com>
2023-10-26 02:13:32 -0400

    added Pokemon list

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'Chicken';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', type: 'Grass', height:'0.7m'},
+    {name: 'Charmander', type: 'Fire', height: '0.6m'},
+    {name: 'Squirtle', type: 'Water', height:'0.5m'}
+];
